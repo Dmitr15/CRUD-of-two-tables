6b30292ffa18678b2fe4216065b4f1c1f414d70c
Ovsyannikov Dmitry <dmitroy1234@gmail.com>
2025-05-14 10:58:10 +0300

    Added functions for adding new product and new supplier

--- DataAction.php
+++ DataAction.php
@@ -1,8 +1,6 @@
 <?php
-
 class DataAction
 {
-
     public static function get_all_products()
     {
         $query = Database::prepare("SELECT * from `product`");
@@ -31,28 +29,30 @@ class DataAction
         return $products;
     }
 
-    public static function add_product(string $sql, array $filters): array
+    public static function add_product(string $img_path, string $name, string $supplier, string $description, int $cost)
     {
-        $query = Database::prepare($sql);
-        foreach ($filters as $k => $v) {
-            $query->bindValue($k, $v);
-        }
-        $query->execute();
-        $products = $query->fetchAll();
+        $query = Database::prepare("INSERT INTO `product` (`img_path`, `name`, `id_supplier`, `description`, `cost`) VALUES (:img_path, :name, :gender, :supplier, :description, :cost)");
+        
+        $query->bindValue(":img_path", $img_path);
+        $query->bindValue(":name", $name);
+        $query->bindValue(":supplier", $supplier);
+        $query->bindValue(":description", $description);
+        $query->bindValue(":cost", $cost);
 
-        return $products;
+        if (!$query->execute()) {
+           throw new PDOException('Exception while adding a new product');
+        }
     }
 
-    public static function add_supplier(string $sql, array $filters): array
+    public static function add_supplier(string $name)
     {
-        $query = Database::prepare($sql);
-        foreach ($filters as $k => $v) {
-            $query->bindValue($k, $v);
-        }
-        $query->execute();
-        $products = $query->fetchAll();
+        $query = Database::prepare("INSERT INTO `supplier` (`namesupplier`) VALUES (:name)");
+        
+        $query->bindValue(":namesupplier", $name);
 
-        return $products;
+        if (!$query->execute()) {
+           throw new PDOException('Exception while adding a new supplier');
+        }
     }
 
     public static function add_relationship(string $sql, array $filters): array
