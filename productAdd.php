497949448c5af23e5bcd444fcd04afb4e3b90e03
Ovsyannikov Dmitry <dmitroy1234@gmail.com>
2025-05-14 12:57:00 +0300

    Added page for adding products

---
+++ productAdd.php
@@ -0,0 +1,54 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+
+require_once("core.php");
+require_once('head.php');
+
+$suppliers = DataAction::get_all_suppliers();
+?>
+<div class="container">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="/Lr6/product.php">Список продуктов</a>
+            </li>
+            <li class="breadcrumb-item active">
+                Новый продукт
+            </li>
+        </ol>
+    </nav>
+
+    <h1>Добавить продукт</h1>
+
+    <div class="alert alert-danger" role="alert">
+
+    </div>
+
+    <form class="row row-cols-lg-auto g-3 align-items-center" method="POST" enctype="multipart/form-data">
+        <div class="col-4">
+            <input type="text" class="form-control" placeholder="Название товара" name="setProductName" value="<?=htmlspecialchars($_POST['setProductName'])?>">
+        </div>
+        <div class="col-4">
+            <input type="text" class="form-control" placeholder="Описание" name="setProductDescription" value="<?=htmlspecialchars($_POST['setProductDescription'])?>">
+        </div>
+        <div class="col-4">
+            <input type="text" class="form-control" placeholder="Стоимость товара" name="setProductPrice" value="<?=htmlspecialchars($_POST['setProductPrice'])?>">
+        </div>
+        <div class="col-4">
+            <input type="file" class="form-control" name="setProductPhoto">
+        </div>
+        <div class="col-4">
+            <select class="form-select" name="setProductSupplier">
+                <option>Бренд</option>
+                <?php
+                foreach ($suppliers as $supplier) {
+                    echo "<option value=" . $supplier['id'] . ">" . $supplier['namesupplier'] . "</option>";
+                }
+                ?>
+            </select>
+        </div>
+        <div class="col-4">
+            <button class="btn btn-primary" type="submit">Отправить</button>
+        </div>
+    </form>
+</div>
