a556bcdceefdab9e2343b0307c44f3cf60e219b0
Ovsyannikov Dmitry <dmitroy1234@gmail.com>
2025-05-16 20:33:29 +0300

    Added addProductField function

--- productAdd.php
+++ productAdd.php
@@ -5,6 +5,8 @@ require_once("core.php");
 require_once('head.php');
 
 $suppliers = DataAction::get_all_suppliers();
+
+$errors = UserAction::addProductField();
 ?>
 <div class="container">
     <nav aria-label="breadcrumb">
@@ -20,26 +22,26 @@ $suppliers = DataAction::get_all_suppliers();
 
     <h1>Добавить продукт</h1>
 
-    <div class="alert alert-danger" role="alert">
-
-    </div>
+    <?php if (!empty($errors)): ?>
+        <div class="alert alert-danger" role="alert"><?= $errors ?></div>
+    <?php endif; ?>
 
     <form class="row row-cols-lg-auto g-3 align-items-center" method="POST" enctype="multipart/form-data">
         <div class="col-4">
-            <input type="text" class="form-control" placeholder="Название товара" name="setProductName" value="<?=htmlspecialchars($_POST['setProductName'])?>">
+            <input type="text" class="form-control" placeholder="Название товара" name="setProductName" value="<?= htmlspecialchars($_POST['setProductName']) ?>">
         </div>
         <div class="col-4">
-            <input type="text" class="form-control" placeholder="Описание" name="setProductDescription" value="<?=htmlspecialchars($_POST['setProductDescription'])?>">
+            <input type="text" class="form-control" placeholder="Описание" name="setProductDescription" value="<?= htmlspecialchars($_POST['setProductDescription']) ?>">
         </div>
         <div class="col-4">
-            <input type="text" class="form-control" placeholder="Стоимость товара" name="setProductPrice" value="<?=htmlspecialchars($_POST['setProductPrice'])?>">
+            <input type="text" class="form-control" placeholder="Стоимость товара" name="setProductPrice" value="<?= htmlspecialchars($_POST['setProductPrice']) ?>">
         </div>
         <div class="col-4">
             <input type="file" class="form-control" name="setProductPhoto">
         </div>
         <div class="col-4">
             <select class="form-select" name="setProductSupplier">
-                <option>Бренд</option>
+                <option value>Бренд</option>
                 <?php
                 foreach ($suppliers as $supplier) {
                     echo "<option value=" . $supplier['id'] . ">" . $supplier['namesupplier'] . "</option>";
